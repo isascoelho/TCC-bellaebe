cb5f129dc7703b98119069999d74fa44b82af529
Isabella Sofia Machado Coelho <isascoelhok@gmail.com>
2026-06-02 16:32:20 -0300

    versão variaveis

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "230508Isa%",
-  database: "smartcash"
+  host:     process.env.MYSQL_HOST,
+  port:     process.env.MYSQL_PORT,
+  user:     process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 db.connect(err => {
