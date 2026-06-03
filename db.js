da3df39ee52db5c1187f3a9daa4b602470757348
Isabella Sofia Machado Coelho <isascoelhok@gmail.com>
2026-06-03 11:54:16 -0300

    versão db

--- db.js
+++ db.js
@@ -1,21 +1,26 @@
 require("dotenv").config();
-
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host:     process.env.MYSQL_HOST,
   port:     process.env.MYSQL_PORT,
   user:     process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("❌ Erro ao conectar no MySQL:", err);
   } else {
     console.log("✅ MySQL conectado com sucesso!");
+    connection.release();
   }
 });
+
+module.exports = db;
 
 module.exports = db;
